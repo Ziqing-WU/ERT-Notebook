e3b635442dabef08127240cfb5226711f2ba8c78
Ziqing-WU <you@example.com>
2021-01-10 13:42:28 +0100

    complete spam section

---
+++ solutions/code1.py
@@ -0,0 +1,18 @@
+spam_ET = ExtraTreesClassifier(n_estimators=200, criterion='entropy')
+spam_ET.fit(Xtrain,ytrain)
+
+spam_ET.score(Xtest.toarray(),ytest)
+
+# Compute cross-validation score
+nb_trials = 20
+score = []
+for i in range(nb_trials):
+    Xtrain, ytrain, Xtest, ytest = spam_data.shuffle_and_split(2000)
+    spam_ET = ExtraTreesClassifier(n_estimators=200, criterion='entropy')
+    spam_ET.fit(Xtrain,ytrain);
+    score += [spam_ET.score(Xtest,ytest)]
+    print('*', end='')
+print(" done!")
+
+print("Average generalization score:", np.mean(score))
+print("Standard deviation:", np.std(score))
